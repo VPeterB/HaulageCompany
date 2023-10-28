79250d3cf6a0d3d1ee00c0c0849a08e907a255f4
Vida Péter <vida.peter1127@gmail.com>
2023-10-28 16:56:48 +0200

    init

---
+++ src/main/java/hu/bme/aut/haulagecompany/HaulageCompanyApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.haulagecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaulageCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaulageCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bme/aut/haulagecompany/HaulageCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.haulagecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulageCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
